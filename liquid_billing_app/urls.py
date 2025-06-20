1b843e306fdc2abc228f6af30f4dc9ee55ff6c68
jescobaras84 <jescobaras@gmail.com>
2025-06-19 18:30:38 -0600

    urls.py

--- liquid_billing_app/urls.py
+++ liquid_billing_app/urls.py
@@ -1,8 +1,11 @@
+# liquid_billing_app/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView # Importa RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')), # Django's auth URLs
-    path('app/', include('invoicing.urls')), # App specific URLs
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('app/', include('invoicing.urls')),
+    path('', RedirectView.as_view(url='/app/', permanent=True)), # ¡Añade esta línea!
 ]
